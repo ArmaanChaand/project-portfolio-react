bf16cde8afe34ed1951688c11ff0f8f721516643
CHAAND <armaan.chaand17@gmail.com>
2023-10-21 15:51:06 +0530

    Update Home.jsx

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -19,11 +19,14 @@ export default function Home(){
         <section className="HOME page-section">
             <div className="intro">
                 <h1 className="main-heading">
-                   I Love You Baby
+                   Baby, suno na ...
                     <br/>
-                    <span className="text-[0.9em] opacity-80">❤️❤️❤️</span>
+                    <span className="text-[0.9em] opacity-80"> hua yeh tha ki ... </span>
                     <br/>
-                    <span className="text-[0.8em] opacity-80">I'm ...</span>
+                    <span className="text-[0.5em] opacity-80">Hm raat mein baat krna chah rahe the ... lekin tm call receive nhi kr rahe the ... toh raat bhar mann mein kchh se kchh chalne laga...
+Aur phir subah bhi call receive nhi kiye toh laga pata nhi kya ho gaya...
+phir chai peete peete woh sab yaad aane laga
+</span>
                 </h1>
                 <h5 class="profession">
                         <motion.span className="different-profs" animate={{y:y?0:100}}>
