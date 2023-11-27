cf8a78d831d1fe3dd19a611b8b4f994e8002a80b
ArmaanChaand <armaan.chaand17@gmail.com>
2023-11-27 19:57:55 +0530

    changes in ps1

--- push_to_github.ps1
+++ push_to_github.ps1
@@ -1,6 +1,10 @@
+# Prompt user for commit message
+$commitMessage = Read-Host "Enter commit message"
+
+# Perform Git operations
 git checkout dev
 git add .
-git commit -m "changes"
+git commit -m "$commitMessage"
 git push origin dev
 git checkout main
 git merge dev
