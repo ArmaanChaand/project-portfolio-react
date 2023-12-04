139f7a0a15577339243a2aa6d48c0e8cd8f24f5f
ArmaanChaand <armaan.chaand17@gmail.com>
2023-12-04 18:14:25 +0530

    projects section

--- src/components/SetSectionWrapper.jsx
+++ src/components/SetSectionWrapper.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { useCommon } from "../context/CommonContext";
 
 export default function SetSectionWrapper({name="", className="", children}){
-    const {set_section_inview} = useCommon()
+    const {section_inview, set_section_inview} = useCommon()
     const  [ref, inView, entry] = useInView({
         threshold:0.5,
     })
@@ -14,8 +14,9 @@ export default function SetSectionWrapper({name="", className="", children}){
     }
     
    }, [inView])
+   
     
-   const tw_classes = `flex flex-col justify-center items-center w-full h-full snap-center`
+   const tw_classes = `flex flex-col justify-center items-center w-full snap-center`
 
     return (
         <section ref={ref} id={`SECTION_ID_${name}`}  className={tw_classes + ' ' + className}>
